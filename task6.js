7f27cc02825819f1c6ce12e9cc6e42e961f1b649
Andrey <56323140+AndreyNazarov@users.noreply.github.com>
2020-10-18 22:48:09 +0200

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,18 @@
+const inputRef = document.querySelector("#validation-input");
+
+inputRef.addEventListener("blur", (event) => {
+  console.dir(event.target);
+
+  if (event.target.value.length === Number(event.target.dataset.length)) {
+    event.target.classList.add("valid");
+    return;
+  } else {
+    event.target.classList.remove("valid")
+    event.target.classList.add("invalid");
+  }
+});
+inputRef.addEventListener("blur", (event) => {
+  if (event.target.value.length === 0) {
+    event.target.classList.remove("valid", "invalid");
+  }
+});
